ba2fc52d32dc5cc52aad7892427359ba12cb0d1d
ZahidRamirez00 <zahidramirez.00@gmail.com>
2023-01-16 12:12:15 +0100

    Aggiungere i file di progetto.

---
+++ Agenzia viaggio mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Agenzia_viaggio_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
