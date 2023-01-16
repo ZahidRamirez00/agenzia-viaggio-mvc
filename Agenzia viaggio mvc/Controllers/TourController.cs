f676d02b19442a8de809698f9bdd88efa57e00a3
AnukaJapa <japaridze.ana96@gmail.com>
2023-01-16 12:48:43 +0100

    creazione controller TourController.cs

---
+++ Agenzia viaggio mvc/Controllers/TourController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Agenzia_viaggio_mvc.Controllers
+{
+    public class TourController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
